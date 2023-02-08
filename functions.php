82c76eddf983cad40edd7d4b115fe59b47f96a24
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-08 18:46:37 +0100

    Inserito filtro ripetizione caratteri

--- functions.php
+++ functions.php
@@ -7,19 +7,27 @@ $numbers = '0123456789';
 $symbols = '!?.-+/';
 $total_char = $words . $up_words . $numbers
     . $symbols;
-function get_random_word($string, $length)
+
+function get_random_string($string, $length, $char_rips = 'yes')
 {
     $random_word = '';
     $max_value = strlen($string);
     do {
         $index = rand(0, $max_value);
-        $random_word .= substr($string, $index, 1);
+        $char = substr($string, $index, 1);
+        if ($char_rips === 'no') {
+            do {
+                $index = rand(0, $max_value);
+                $char = substr($string, $index, 1);
+            } while (str_contains($random_word, $char));
+        }
+        $random_word .= $char;
     } while (strlen($random_word) < $length);
     return $random_word;
 }
 if ($password_length) {
     session_start();
-    $password = get_random_word($total_char, $password_length);
+    $password = get_random_string($total_char, $password_length, $char_rips);
     $_SESSION['password'] = $password;
     header('Location: password_page.php');
 
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$char_rips = $_GET['char-rips'] ?? 'yes';
 include('functions.php');
 ?>
 
@@ -14,19 +15,44 @@ include('functions.php');
     </header>
     <main>
       <h2 class="text-white text-center mb-4">Genera una password sicura</h2>
-      <form action="">
-        <div class="d-flex justify-content-between align-items-center">
-          <label for="password-length" class="form-label">Lunghezza password:</label>
-          <input type="number" name="password-length" id="password-length" class="form-control w-25" min="1"
-            value="<?= $password_length ?>">
+      <form action="#">
+        <div class="row row-cols-2 justify-content-between align-items-center mb-4">
+          <div class="col">
+            <label for="password-length" class="form-label">Lunghezza password:</label>
+          </div>
+          <div class="col">
+            <input type="number" name="password-length" id="password-length" class="form-control w-75" min="1" max='68'
+              value="<?= $password_length ?>">
+          </div>
         </div>
-        <div class="mt-3">
-          <button class="btn btn-primary ">Invia</button>
-          <a href="index.php" class="btn btn-secondary">Annulla</a>
-        </div>
-      </form>
-    </main>
-  </div>
-</body>
+        <div class="row row-cols-2 justify-content-between">
+          <p>Consenti ripetizioni di uno o più caratteri:</p>
+          <div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="char-rips" value='yes' id="yes" <?php
+              if ($char_rips === 'yes')
+                echo 'checked' ?>>
+                <label class="form-check-label" for="yes">
+                  Si
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="char-rips" id="no" value='no' <?php
+              if ($char_rips === 'no')
+                echo 'checked' ?>>
+                <label class="form-check-label" for="no">
+                  No
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="mt-3">
+            <button class="btn btn-primary me-3">Invia</button>
+            <a href="index.php" class="btn btn-secondary">Annulla</a>
+          </div>
+        </form>
+      </main>
+    </div>
+  </body>
 
-</html>
+  </html>
